db5dd6dbef735285d51fb7d273476f5439af63ea
John Sylvain <john.n.sylvain@gmail.com>
2019-10-30 06:48:30 -0500

    FEAT: update button animation

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -20,20 +20,27 @@ const InnerCircle = styled.div`
   transition: 300ms ease-in-out;
   transform: scale(1);
   z-index: 1;
-  opacity: 0.4;
+  opacity: 0.2;
+
+  &:hover {
+    opacity: 0.4;
+  }
 
   &:after {
     content: '';
     width: 20px;
     height: 20px;
     border-radius: 50%;
-    border: 2px dotted #2c2c2c;
+    box-shadow: -15px 0px 0px -8px white, 15px 0px 0px -8px white,
+      0px -15px 0px -8px white, 0px 15px 0px -8px white,
+      -11px 11px 0px -8px white, 11px 11px 0px -8px white,
+      -11px -11px 0px -8px white, 11px -11px 0px -8px white;
     position: absolute;
     top: 0px;
     left: 0px;
-    transform: scale(0.5);
-    transition: 300ms ease-in-out;
-    z-index: -100;
+    transform: scale(0.2);
+    transition: 300ms cubic-bezier(0.49, 0.22, 0.47, 1.17);
+    opacity: 0;
   }
 
   &:before {
@@ -62,8 +69,8 @@ const InnerCircle = styled.div`
       }
 
       &:after {
-        transform: scale(2);
-        border-color: white;
+        transform: scale(1);
+        opacity: 1;
       }
     `}
 `;
