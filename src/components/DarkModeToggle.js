44910943330aac51b13f9438413306857e5039d5
John Sylvain <john.n.sylvain@gmail.com>
2019-11-02 15:46:46 -0500

    FEAT: use svgs for dark mode toggle

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,89 +1,24 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import useDarkMode from '../hooks/useDarkMode';
+import { Sun, Moon } from './Icon';
 
 const Toggle = styled.button`
-  width: 20px;
-  height: 20px;
+  opacity: ${props => (props.darkMode ? 0.5 : 0.2)};
   background: none;
   border: none;
   outline: none;
   cursor: pointer;
-`;
-
-const InnerCircle = styled.div`
-  border-radius: 50%;
-  background: #2c2c2c;
-  width: 20px;
-  height: 20px;
-  position: relative;
-  transition: 300ms ease-in-out;
-  transform: scale(1);
-  z-index: 1;
-  opacity: 0.2;
-
-  &:hover {
-    opacity: 0.4;
-  }
-
-  &:after {
-    content: '';
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    box-shadow: -15px 0px 0px -8px white, 15px 0px 0px -8px white,
-      0px -15px 0px -8px white, 0px 15px 0px -8px white,
-      -11px 11px 0px -8px white, 11px 11px 0px -8px white,
-      -11px -11px 0px -8px white, 11px -11px 0px -8px white;
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    transform: scale(0.2);
-    transition: 300ms cubic-bezier(0.49, 0.22, 0.47, 1.17);
-    opacity: 0;
-  }
-
-  &:before {
-    content: '';
-    width: 20px;
-    height: 20px;
-    background: white;
-    position: absolute;
-    top: -8px;
-    left: 8px;
-    border-radius: 50%;
-    z-index: 3;
-    transition: 300ms ease-in-out;
-    transform: scale(1);
-  }
-
-  ${props =>
-    props.darkMode &&
-    css`
-      background: white;
-      transform: scale(0.7);
-
-      &:before {
-        background: #2c2c2c;
-        transform: scale(0);
-      }
-
-      &:after {
-        transform: scale(1);
-        opacity: 1;
-      }
-    `}
+  color: inherit;
 `;
 
 const DarkModeToggle = () => {
   const [darkMode, setDarkMode] = useDarkMode();
 
   return (
-    <div>
-      <Toggle onClick={() => setDarkMode(!darkMode)}>
-        <InnerCircle darkMode={darkMode} />
-      </Toggle>
-    </div>
+    <Toggle onClick={() => setDarkMode(!darkMode)} darkMode={darkMode}>
+      {darkMode ? <Sun /> : <Moon />}
+    </Toggle>
   );
 };
 
